4c8a0788bd1e582298c62eeb0565ed61a8847899
DANIEL-DEMELASH <demelashdaniel5@gmail.com>
2024-07-25 15:32:57 +0300

    style: remove todo comment

--- lib/features/posts/data/repository/post_repository.dart
+++ lib/features/posts/data/repository/post_repository.dart
@@ -10,7 +10,6 @@ class PostRepositoryImp extends PostRepository {
   
   @override
   Future<DataState<List<PostModel>>> getPosts() async{
-    //Todo: implement api request to data source
     try {
       List<PostModel> response = await _postAPIService.getPosts();
       return DataSuccess(response);
